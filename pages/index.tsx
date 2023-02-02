047036037964fdd641dce2cb604ca78047924586
Arthur Bryan <arthurbryansk@gmail.com>
2023-02-01 21:46:25 -0300

    refatoração de pastas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import CountryCard from "@/components/country-card/CountryCard";
-import useFetch from "@/hooks/useFetch";
+import useFetch from "@/src/hooks/useFetch";
 
 export default function Home() {
 
