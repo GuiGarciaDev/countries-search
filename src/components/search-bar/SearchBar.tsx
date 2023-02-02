e56c5a4d11564b41f04434cd3445d5dc7265b7e3
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2023-02-02 18:17:00 -0300

    Main page styles
    Changed main page styles
    Refactor of search bar component
    Added dark theme

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -5,12 +5,11 @@ function SearchBarComponent() {
   return (
     <>
       <div className={styles.container}>
-        <IoMdSearch
-          className={styles.search_icon}
-          fontSize={25}
-        ></IoMdSearch>
+        <label htmlFor="searchbar">
+          <IoMdSearch fontSize={25}/>
+        </label>
         <input
-          className={styles.search_bar_component}
+          id="searchbar"
           type="text"
           placeholder="Search for a country..."
         ></input>
