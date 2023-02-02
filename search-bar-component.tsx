2f40719085b2142e871711dc7d175a6c044ccdba
Arthur Bryan <arthurbryansk@gmail.com>
2023-02-01 21:40:28 -0300

    update search-bar-component

---
+++ search-bar-component.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles/search-bar-component.module.scss";
+import { IoMdSearch } from 'react-icons/io';
+
+function SearchBarComponent() {
+  return (
+    <>
+          <IoMdSearch width={50} color={"black"}></IoMdSearch>
+      <div>
+        <input
+          className={styles.search_bar_component}
+          type="text"
+          placeholder="Search for a country..."
+          >
+
+          </input>
+      </div>
+    </>
+  );
+}
+
+export default SearchBarComponent;
