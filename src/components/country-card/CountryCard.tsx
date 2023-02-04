91327e1f04524b2fb1d20b301c00cffb36bc641c
Arthur Bryan <arthurbryansk@gmail.com>
2023-02-04 18:09:41 -0300

    country card component

--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -1,9 +1,11 @@
 import dotsFormater from '@/src/utils/dots';
 import styles from '@/styles/country-card/CountryCard.module.scss';
+import Link from 'next/link';
 
 export interface Icountry {
     country: {
         name: string,
+        alpha2Code: string,
         flag: string,
         population: string,
         region: string,
@@ -13,16 +15,18 @@ export interface Icountry {
 
 export default function CountryCard({ country } : Icountry) {
     return (
-        <div className={styles.countryCard}>
-            <div className={styles.imgHolder}>
-                <img src={country.flag} alt='country-flag'/>
+        <Link href={`country/${country.alpha2Code.toLocaleLowerCase()}`}>
+            <div className={styles.countryCard}>
+                <div className={styles.imgHolder}>
+                    <img src={country.flag} alt='country-flag'/>
+                </div>
+                <div className={styles.countryInfo}>
+                    <h1>{country?.name}</h1>
+                    <span><b>Population:</b> {dotsFormater(country.population)}</span>
+                    <span><b>Region:</b> {country.region}</span>
+                    <span><b>Capital:</b> {country.capital ? country.capital : 'Undefined'}</span>
+                </div>
             </div>
-            <div className={styles.countryInfo}>
-                <h1>{country?.name}</h1>
-                <span><b>Population:</b> {dotsFormater(country.population)}</span>
-                <span><b>Region:</b> {country.region}</span>
-                <span><b>Capital:</b> {country.capital ? country.capital : 'Undefined'}</span>
-            </div>
-        </div>   
+        </Link>
     )
 }
