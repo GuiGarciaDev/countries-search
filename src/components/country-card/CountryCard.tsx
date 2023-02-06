6bf4ca4ff84643ab23c9171b3cb4c329d0cbbf5a
Arthur Bryan <arthurbryansk@gmail.com>
2023-02-06 19:03:43 -0300

    added animation on cards

--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -1,32 +1,45 @@
-import dotsFormater from '@/src/utils/dots';
-import styles from '@/styles/country-card/CountryCard.module.scss';
-import Link from 'next/link';
+import dotsFormater from "@/src/utils/dots";
+import styles from "@/styles/country-card/CountryCard.module.scss";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export interface Icountry {
-    country: {
-        name: string,
-        alpha2Code: string,
-        flag: string,
-        population: string,
-        region: string,
-        capital: string,
-    }
+  country: {
+    name: string;
+    alpha2Code: string;
+    flag: string;
+    population: string;
+    region: string;
+    capital: string;
+  };
 }
 
-export default function CountryCard({ country } : Icountry) {
-    return (
-        <Link href={`country/${country.alpha2Code.toLocaleLowerCase()}`}>
-            <div className={styles.countryCard}>
-                <div className={styles.imgHolder}>
-                    <img src={country.flag} alt='country-flag'/>
-                </div>
-                <div className={styles.countryInfo}>
-                    <h1>{country?.name}</h1>
-                    <span><b>Population:</b> {dotsFormater(country.population)}</span>
-                    <span><b>Region:</b> {country.region}</span>
-                    <span><b>Capital:</b> {country.capital ? country.capital : 'Undefined'}</span>
-                </div>
-            </div>
-        </Link>
-    )
+export default function CountryCard({ country }: Icountry) {
+  return (
+    <motion.div
+      className="container"
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 1 }}
+    >
+      <Link href={`country/${country.alpha2Code.toLocaleLowerCase()}`}>
+        <div className={styles.countryCard}>
+          <div className={styles.imgHolder}>
+            <img src={country.flag} alt="country-flag" />
+          </div>
+          <div className={styles.countryInfo}>
+            <h1>{country?.name}</h1>
+            <span>
+              <b>Population:</b> {dotsFormater(country.population)}
+            </span>
+            <span>
+              <b>Region:</b> {country.region}
+            </span>
+            <span>
+              <b>Capital:</b> {country.capital ? country.capital : "Undefined"}
+            </span>
+          </div>
+        </div>
+      </Link>
+    </motion.div>
+  );
 }
