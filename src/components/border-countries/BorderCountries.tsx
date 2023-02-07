b8d9f9f48d0b6afcabcc1deee3313e27a87e768f
Arthur Bryan <arthurbryansk@gmail.com>
2023-02-06 21:25:28 -0300

    Update BorderCountries.tsx

--- src/components/border-countries/BorderCountries.tsx
+++ src/components/border-countries/BorderCountries.tsx
@@ -1,10 +1,13 @@
 import styles from "@/styles/border-countries/BorderCountries.module.scss";
+import BorderCoutriesAnimation from "../country-page-motion-divs/BorderCountriesAnimation";
 
 function BorderCountries({ name }: any) {
   return (
+    <BorderCoutriesAnimation>
       <span className={styles.border_countries}>
         <p>{name}</p>
       </span>
+    </BorderCoutriesAnimation>
   );
 }
 
